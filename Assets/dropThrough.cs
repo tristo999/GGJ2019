7b922c76f775c45a695fba1f819f10d04e92cecd
grumbleBee <s.kyne96@gmail.com>
2019-01-26 18:51:18 -0500

    Jump Through/Fall Through Platforms
    found in sarahjail

---
+++ Assets/dropThrough.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropThrough : MonoBehaviour
+{
+    Collider2D coll;
+
+    void Awake()
+    {
+        coll = gameObject.GetComponent<Collider2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetAxis("Vertical") < 0){
+            coll.isTrigger = true;
+        } 
+        else{
+            coll.isTrigger = false;
+        }
+    }
+
+}
