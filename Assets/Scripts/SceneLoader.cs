c45321b737f2a23302bb7449f83cedd4cf862326
grumbleBee <s.kyne96@gmail.com>
2019-01-27 18:25:39 -0500

    menu tweaks
    main menu buttons work now (AI & cutscene stuff still need to be added to lvl1)

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿     using UnityEngine;
+     using UnityEngine.SceneManagement;
+     using System.Collections;
+     
+     public class SceneLoader: MonoBehaviour {
+     
+          public void LoadScene(int level)
+          { 
+             SceneManager.LoadScene(level);
+           }
+     }
